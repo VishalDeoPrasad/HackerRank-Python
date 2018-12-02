408d61df3f7ac606a1983300e17792a0bdbd83e7
Vishal Deo Prasad <44454324+VishalDeoPrasad@users.noreply.github.com>
2018-12-02 13:33:17 +0530

    Add files via upload

--- Staircase.py
+++ Staircase.py
@@ -1,3 +1,30 @@
+
+"""print this pattern
+      *
+     **
+    ***
+   ****
+
+"""
+"""n=4
+for i in range(n):
+	for j in range(n):
+		if i+j>=n-1:
+			print "#"
+		else:
+			print " """
+
+
+"""n=4
+for i in range(n):
+    #print('{:>{len}}'.format('#'*(i+1), len=n))
+    print('{:>4}'.format('*'*(i+1)))
+ """
+
 n=input("Enter Number to Stairs:")
 for i in range(1,n+1):
     print " "*(n-i) + "#"*i
+
+
+
+
