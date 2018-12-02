cb9e85bedd2e72edd583b6fd868ad3ddd1cb26d3
Vishal Deo Prasad <44454324+VishalDeoPrasad@users.noreply.github.com>
2018-12-02 13:37:30 +0530

    Update Staircase.py

--- Staircase.py
+++ Staircase.py
@@ -1,30 +1,4 @@
 
-"""print this pattern
-      *
-     **
-    ***
-   ****
-
-"""
-"""n=4
-for i in range(n):
-	for j in range(n):
-		if i+j>=n-1:
-			print "#"
-		else:
-			print " """
-
-
-"""n=4
-for i in range(n):
-    #print('{:>{len}}'.format('#'*(i+1), len=n))
-    print('{:>4}'.format('*'*(i+1)))
- """
-
-n=input("Enter Number to Stairs:")
-for i in range(1,n+1):
-    print " "*(n-i) + "#"*i
-
-
-
-
+n=input("Enter Number to Stairs:") #Taking input from user)
+for i in range(1,n+1):             #Loop from 1 to n+1
+    print " "*(n-i) + "#"*i        #concatenate space and #
