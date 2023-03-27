f10557747490217977ecc9905df12621196ce0ec
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2023-03-27 13:10:14 +0530

    Drawing Book

--- Drawing Book.py
+++ Drawing Book.py
@@ -1,7 +1,14 @@
-def pageCount(n, p):
+def pageCount1(n, p):
     # Write your code here
     front_turn = int((p/2)+1-1 if p%2 == 0 else ((p+1)/2)-1)
     back_turn = int((n-p)/2) if n%2 != 0 else int((n-p+1)/2)
     return min(front_turn, back_turn)
+
+def pageCount(n, p):
+    if n%2==0:
+        n = n+1
+    left = p//2
+    right = (n-p)//2
+    return min(left, right)
 
 print(pageCount(7, 3))
