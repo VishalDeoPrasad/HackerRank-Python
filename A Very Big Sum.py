bbd7417a003707e016ab497b5aff2d5a2e2b228c
Vishal Kumar <vishaldeoprasad@yahoo.com>
2023-11-22 21:15:36 +0530

    Update A Very Big Sum.py

--- A Very Big Sum.py
+++ A Very Big Sum.py
@@ -4,4 +4,4 @@ def aVeryBigSum(ar):
     [bigSum := bigSum+i for i in ar]
     return bigSum
 ar=[1000000001, 1000000002, 1000000003, 1000000004, 1000000005]
-print(aVeryBigSum(ar))
+print(aVeryBigSum(ar)) 
